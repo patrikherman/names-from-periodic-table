6aaef4bf35c3ea8394dabf6d13be241943015fdf
Patrik Herman <patrikherman1@gmail.com>
2022-08-30 20:27:41 +0200

    Added fetch of data in parsing_function.js

--- website/parsing_function.js
+++ website/parsing_function.js
@@ -4,3 +4,9 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
 
+request("https://www.usna.edu/Users/cs/roche/courses/s15si335/proj1/files.php%3Ff=names.txt.html", (error, response, html) => 
+{
+    if(!error && response.statusCode == 200){
+        const $= cheerio.load(html);
+    }
+})
